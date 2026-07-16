b915c25117290178fab8773047e53878dda26ae9
AshishShenoy5103 <ashishshenoy510@gmail.com>
2026-07-16 11:21:15 +0530

    updated UserLoginDTO

--- src/main/java/com/example/payment/dto/UserLoginDTO.java
+++ src/main/java/com/example/payment/dto/UserLoginDTO.java
@@ -5,26 +5,14 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public class UserLoginDTO {
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid Email")
-    private String email;
+public record UserLoginDTO(
 
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters")
-    private String password;
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid Email")
+        String email,
 
-    public UserLoginDTO() {
-    }
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, message = "Password must be at least 8 characters")
+        String password
 
-    public UserLoginDTO(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {return email;}
-    public void setEmail(String email) {this.email = email;}
-
-    public String getPassword() {return password;}
-    public void setPassword(String password) {this.password = password;}
-}
+) {}
